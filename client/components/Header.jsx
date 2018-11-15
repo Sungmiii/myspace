bcb377d8063f9968b31b08cc24c36d0dfefedcc3
Sungmiii <christyksmile@gmail.com>
2018-11-16 09:05:17 +1300

    Merge version 1

---
+++ client/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Nav from './Nav'
+
+// import Logo from '../../server/public/img/logo.gif'
+const Logo = require('svg-inline-loader?./img/logo.svg')
+
+
+const Header = () => {
+    return (
+        <header className="heroarea">
+            <div className="headercontent">
+                <Nav />
+                <Logo src={require('svg-inline-loader?./img/logo.svg')} />
+                <h1>My Space</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header
+
---
+++ client/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Weather from './Weather'
+
+const Home = () => {
+    return (
+        <main>
+            <section className="mainarea">
+                <p>all api area</p>
+                <Weather />
+            </section>
+        </main>
+    )
+}
+
+export default Home
