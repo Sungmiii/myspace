9506cabd8e0e3b6c62b4212d0c1a07c9e52d38f1
Sungmiii <christyksmile@gmail.com>
2018-11-15 18:08:42 +1300

    check app working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <h1>This my Space</h1>
+    )
+  }
 }
 
 export default App
