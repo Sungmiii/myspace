bcaa3ffb698303d49b9d23a3450ab1b966cc5106
Joan-SHI <qiongnashi@gmail.com>
2018-11-16 15:43:28 +1300

    tests work

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,6 +2,10 @@ import React from 'react'
 import {shallow} from 'enzyme'
 
 import App from '../client/components/App'
+import Header from '../client/components/Header';
+import LiveStream from '../client/components/LiveStream';
+import Weather from '../client/components/LiveStream';
+
 
 test('<App />', () => {
   const expected = 1
@@ -9,3 +13,32 @@ test('<App />', () => {
   const actual = wrapper.find('div.gridcontainer').length
   expect(actual).toBe(expected)
 })
+
+test('<App />', () => {
+  const expected = 1
+  const wrapper = shallow(<Header />)
+  const actual = wrapper.find('div').length
+  expect(actual).toBe(expected)
+})
+
+test('<App />', () => {
+  const expected = 1
+  const wrapper = shallow(<LiveStream />)
+  const actual = wrapper.find('h2').length
+  expect(actual).toBe(expected)
+})
+
+test('<App />', () => {
+  const expected = 1
+  const wrapper = shallow(<Weather />)
+  const actual = wrapper.find('div').length
+  expect(actual).toBe(expected)
+})
+
+
+
+
+
+
+
+
